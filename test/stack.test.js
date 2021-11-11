5cf3cb92df63abefad0afa467296949e207b02d3
mhk-local <superuser.mhk@gmail.com>
2021-11-11 09:00:55 +0300

    init commit:

---
+++ test/stack.test.js
@@ -0,0 +1,40 @@
+class Stack {
+    constructor() {
+        this.top = -1;
+        this.items = {};
+    }
+
+    get peek() {
+        return this.items[this.top];
+
+    }
+
+    push(value) {
+        this.top += 1;
+        this.items[this.top] = value;
+    }
+}
+
+describe('My Stack', () => {
+
+    let stack;
+
+    beforeEach(() => {
+        stack = new Stack();
+    });
+
+    it('is created empty', () => {
+        const stack = new Stack();
+
+        expect(stack.top).toBe(-1);
+        expect(stack.items).toEqual({});
+    } );
+
+    it('can push to the top', () => {
+        stack.push("item1")
+        expect(stack.top).toBe(0);
+        expect(stack.peek).toBe('item1');
+    });
+
+    it.todo('can pop off');
+})
